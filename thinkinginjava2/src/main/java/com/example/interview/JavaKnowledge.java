edbafd547453f5886406dcd29601338cc27ff443
zhongyao <zhongyao@alibaba-inc.com>
2019-01-14 14:54:08 +0800

    String StringBuffer StringBuilder区别

--- thinkinginjava2/src/main/java/com/example/interview/JavaKnowledge.java
+++ thinkinginjava2/src/main/java/com/example/interview/JavaKnowledge.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 public class JavaKnowledge {
     public static void main(String args[]) {
         /**
-         * 1、"=="与"equals":
+         * 一、"=="与"equals":
          * "=="如果是基本值类型，表示值是否相同；如果是对象表示对象地址是否相同
          * "equals"表示两个对象的内容是否相同
          */
@@ -19,7 +19,7 @@ public class JavaKnowledge {
 
         int i = 2;
         int j = 2;
-        HashMap<String,String> hashMap = new HashMap<>();
+        HashMap<String, String> hashMap = new HashMap<>();
         String a = new String("hello");
         String b = new String("hello");
         //out(a == b);//true
@@ -29,6 +29,44 @@ public class JavaKnowledge {
         out(b.hashCode());
         out(i == j);
 
+        /**
+         * 三、wait()与sleep()的区别：
+         * 1、wait()会释放对象锁，直到调用notify()方法
+         * 2、而sleep()不会释放对象锁
+         */
+
+        /**
+         * 四、String str = new String("abc");
+         * String str = "abc";的区别:
+         *
+         * 第一个是创建了两个对象:一个"abc"对象 一个new String("abc")产生的对象
+         * 第二个是创建了一个对象
+         */
+        String str = new String("abc");
+        String str2 = "abc";
+        out(str.equals(str2));
+        out(str == str2);
+
+        /**
+         * 五、如何用String str1 = "abc"，String str2 = "def"，String str3 = "ghi"得到"abcdefghi"字符串？
+         * 如果使用String str = str1+str2+str3的方式，不是一个好的方式，因为在字符串常量池中会产生不必要的字符串
+         * 最好的方式是使用StringBuilder
+         *
+         * String StringBuffer StringBuilder的区别：
+         * String是字符串常量
+         * StringBuffer是字符串变量（线程安全）
+         * StringBuilder是字符串变量（非线程安全）
+         */
+        StringBuilder builder = new StringBuilder();
+        builder.append("abc");
+        builder.append("def");
+        builder.append("ghi");
+        out(builder.toString());
+
+    }
+
+    private static void out(String string) {
+        System.out.println(string);
     }
 
     private static void out(boolean result) {
@@ -39,6 +77,9 @@ public class JavaKnowledge {
         System.out.println(string);
     }
 
+    /**
+     * 二、hashCode与equals的区别：
+     */
     /**
      * 是对两个对象的地址值进行的比较（即比较引用是否相同）
      *
@@ -66,4 +107,5 @@ public class JavaKnowledge {
     public int hashCode() {
         return super.hashCode();
     }
+
 }
