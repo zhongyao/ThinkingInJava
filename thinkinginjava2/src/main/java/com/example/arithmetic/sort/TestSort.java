179ea0a8930a812ae0e1899b01c58e0e999d0a72
hongri <1285052668@qq.com>
2019-06-09 10:33:44 +0800

    快速排序--挖坑法

--- thinkinginjava2/src/main/java/com/example/arithmetic/sort/TestSort.java
+++ thinkinginjava2/src/main/java/com/example/arithmetic/sort/TestSort.java
@@ -2,16 +2,17 @@ package com.example.arithmetic.sort;
 
 /**
  * Created by zhongyao on 2019-06-09.
+ * 参考：
+ * http://ju.outofmemory.cn/entry/372908
  */
 public class TestSort {
 
-    private int[] array = {32, 43, 23, 13, 5, 20};
-
     public static void main(String[] args) {
         /**
-         * 最终排序结果应为：5 13 20 23 32 43
+         * 最终排序结果应为：5 13 20 22 28 43
          */
-        int[] array = {32, 43, 23, 13, 5, 20};
+        int[] array = {22, 43, 28, 13, 5, 20};
+        int[] sortedArray;
 
         /**
          * 冒泡排序
@@ -29,8 +30,58 @@ public class TestSort {
          * O(1)
          *
          */
-        int[] resultArray = testBubbleSort(array);
-        print(resultArray);
+        //sortedArray = testBubbleSort(array);
+        //print(sortedArray);
+
+        /**
+         * 快速排序(冒泡排序的一种改进)：
+         * 通过一趟划分将要排序序列分割成独立的三个部分：
+         * 左部、基准值、右部
+         * 其中左部所有的数据都比基准值小，右部所有数据都比基准值大，然后再按此方法分别对左部和右部再进行划分。
+         *
+         */
+        sortedArray = testFastSort(array, 0, array.length - 1);
+        print(sortedArray);
+
+    }
+
+    /**
+     *
+     * @param array 待排序数组
+     * @param low 左边界
+     * @param high 右边界
+     * @return
+     */
+    private static int[] testFastSort(int[] array, int low, int high) {
+        if (array == null || array.length <= 0) {
+            return null;
+        }
+
+        if (low >= high) {
+            return null;
+        }
+
+        int left = low;
+        int right = high;
+        int temp = array[left];
+
+        while (left < right) {
+            while (left < right && array[right] >= temp) {
+                right--;
+            }
+            array[left] = array[right];
+
+            while (left < right && array[left] <= temp) {
+                left++;
+            }
+            array[right] = array[left];
+        }
+
+        array[left] = temp;
+        testFastSort(array, low, left - 1);
+        testFastSort(array, left + 1, high);
+
+        return array;
     }
 
     private static int[] testBubbleSort(int[] array) {
@@ -57,6 +108,10 @@ public class TestSort {
     }
 
     private static void print(int[] array) {
+
+        if (array == null) {
+            System.out.print("数组为null");
+        }
         System.out.println("最终排序结果为：");
         for (int num : array) {
             System.out.print(num + " ");
